b975be10c211e0f7d8616d74061ed8649a04a12b
Mohamed_habib <chaiebmohamedhabib1@gmail.com>
2026-01-03 19:55:37 +0100

    use before Each to rempve duplication

--- ecommerce-project/src/Pages/home/product.test.jsx
+++ ecommerce-project/src/Pages/home/product.test.jsx
@@ -1,4 +1,4 @@
-import { it, expect, describe, vi } from 'vitest';
+import { it, expect, describe, vi, beforeEach } from 'vitest';
 import { Product } from './Product';
 import userEvent from '@testing-library/user-event';
 import axios from 'axios';
@@ -8,54 +8,45 @@ import '@testing-library/jest-dom';
 vi.mock('axios');
 
 describe('Product Component', () => {
+  let product;
+  let loadCart;
 
-  const product = {
-    id: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating: {
-      stars: 4.5,
-      count: 87,
-    },
-    priceCents: 1090,
-    keywords: ['socks', 'sports', 'apparel'],
-  };
+  beforeEach(() => {
+    product = {
+      id: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+      image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
+      name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+      rating: {
+        stars: 4.5,
+        count: 87,
+      },
+      priceCents: 1090,
+      keywords: ['socks', 'sports', 'apparel'],
+    };
+    loadCart = vi.fn();
+  });
 
   it('displays the product details correctly', () => {
-    const loadCart = vi.fn();
-
     render(<Product product={product} loadCart={loadCart} />);
 
     expect(
       screen.getByText('Black and Gray Athletic Cotton Socks - 6 Pairs')
     ).toBeInTheDocument();
 
-    expect(
-      screen.getByTestId('product-image')
-    ).toHaveAttribute(
-      'src',
-      'images/products/athletic-cotton-socks-6-pairs.jpg'
-    );
+    expect(screen.getByTestId('product-image'))
+      .toHaveAttribute('src', product.image);
 
-    expect(
-      screen.getByTestId('product-rating-stars')
-    ).toHaveAttribute(
-      'src',
-      'images/ratings/rating-45.png'
-    );
+    expect(screen.getByTestId('product-rating-stars'))
+      .toHaveAttribute('src', 'images/ratings/rating-45.png');
   });
 
   it('adds a product to the cart', async () => {
-    const loadCart = vi.fn();
     const user = userEvent.setup();
-
-    // mock axios response
-    (axios.post).mockResolvedValue({});
+    (axios.post ).mockResolvedValue({});
 
     render(<Product product={product} loadCart={loadCart} />);
 
-    const addToCartButton = screen.getByTestId('add-to-cart-button');
-    await user.click(addToCartButton);
+    await user.click(screen.getByTestId('add-to-cart-button'));
 
     expect(axios.post).toHaveBeenCalledWith(
       'api/cart-items',
